dedd1ec44c786e50e9c43556585edad3c24a4353
lazyat <daniel.courcel@outlook.fr>
2018-03-21 16:30:35 +0100

    first commit

---
+++ Assets/anim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class anim : MonoBehaviour {
+
+	public GameObject mortyClean;
+	public GameObject mortyDab;
+	public AnimatorController mortyDabCtrl;
+	public GameObject mortyDance;
+	public AnimatorController mortyDanceCtrl;
+	public GameObject mortyMarche;
+	public AnimatorController mortyMarcheCtrl;
+	public GameObject mortyTombe;
+	public AnimatorController mortyTombeCtrl;
+	public GameObject mortyAssoir;
+	public AnimatorController mortyAssoirCtrl;
+	public GameObject mortyRecevoir;
+	public AnimatorController mortyRecevoirCtrl;
+	public GameObject mortyLance;
+	public AnimatorController mortyLanceCtrl;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(clickButton){
+			mortydab.GetComponent<Animator>().Controller = mortydabCtrl;
+		}*/
+	}
+}
