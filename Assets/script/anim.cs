96e5b6773708b3c083424c2bc1c29032ea297c36
lazyat <daniel.courcel@outlook.fr>
2018-03-26 14:19:31 +0200

    jesaispasquoidire

--- Assets/script/anim.cs
+++ Assets/script/anim.cs
@@ -13,15 +13,17 @@ public class anim : MonoBehaviour {
 	private Vector3 portalVG, portalVD, mortyVG, mortyVD;
 	private Quaternion base0;
 
+	private bool launched = false;
+
 	// Use this for initialization
 	void Start () {
 		portalVG = new Vector3(2.5f, 2f, 5f);
 		portalVD = new Vector3(-2.5f, 2f, 5f);
-		mortyVG = new Vector3(2.5f, 0.1f, 4.3f);
-		mortyVD = new Vector3(-2.5f, 0.1f, 4.3f);
+		mortyVG = new Vector3(2.5f, 0.1f, 4f);
+		mortyVD = new Vector3(-2.5f, 0.1f, 4f);
 		base0 = new Quaternion();
 
-		MakeAnim(mortyMarche, mortyLance);
+		MakeAnim(mortyDab, mortyDab);
 	}
 
 	void MakeAnim (GameObject morty1, GameObject morty2){
@@ -35,7 +37,20 @@ public class anim : MonoBehaviour {
 		Destroy(portalD, 1);
 	}
 	
+	void move (GameObject morty1, int dg){
+		if(dg == 1){
+			morty1.GetComponent<Transform>().Translate(0,0,Time.deltaTime);
+		}else{
+			morty1.GetComponent<Transform>().Translate(0,0,Time.deltaTime);
+		}
+
+	}
+
 	// Update is called once per frame
 	void Update () {
+
+		if (!launched){
+			move(mortyG, 1);
+		}
 	}
 }
